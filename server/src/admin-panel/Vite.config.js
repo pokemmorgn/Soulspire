4c6737a604a40ca419bc4574b8d9b03bffed6509
pokemmorgn <pokemmorgn@gmail.com>
2025-09-11 18:29:15 +0200

    Create Vite.config.js

---
+++ server/src/admin-panel/Vite.config.js
@@ -0,0 +1,14 @@
+export default {
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: '../server/dist/admin'
+  }
+}
