dedfd2c61d4821e8430401e5e709d56a38224efd
pokemmorgn <pokemmorgn@gmail.com>
2025-09-30 21:23:36 +0200

    Create BannerPity.ts

---
+++ server/src/models/BannerPity.ts
@@ -0,0 +1,67 @@
+// server/src/models/BannerPity.ts
+
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IBannerPity {
+  playerId: string;
+  bannerId: string;
+  pullsSinceLegendary: number;
+  pullsSinceEpic: number;
+  totalPulls: number;
+  hasReceivedLegendary: boolean;
+  lastPullDate: Date;
+}
+
+interface IBannerPityDocument extends Document {
+  playerId: string;
+  bannerId: string;
+  pullsSinceLegendary: number;
+  pullsSinceEpic: number;
+  totalPulls: number;
+  hasReceivedLegendary: boolean;
+  lastPullDate: Date;
+}
+
+const bannerPitySchema = new Schema<IBannerPityDocument>({
+  playerId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  bannerId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  pullsSinceLegendary: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  pullsSinceEpic: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  totalPulls: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  hasReceivedLegendary: {
+    type: Boolean,
+    default: false
+  },
+  lastPullDate: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true,
+  collection: 'banner_pity'
+});
+
+// Index composé pour unicité
+bannerPitySchema.index({ playerId: 1, bannerId: 1 }, { unique: true });
+
+export default mongoose.model<IBannerPityDocument>("BannerPity", bannerPitySchema);
